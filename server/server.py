b7c41976db1add661b827d2fe0d9633e8158b450
maigre <thomas.bohl@gmail.com>
2016-10-25 20:32:50 +0200

    snapshot

--- server/server.py
+++ server/server.py
@@ -1,16 +1,29 @@
 # coding=utf-8
 
+import os
 import requests
 import json
 import dateutil.parser
+import datetime
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
-from flask import Flask
+SNAPSHOT_NAME = 'snapshot.jpg'
+
+# Flask Webserver
 webapp = Flask(__name__)
+webapp.config['UPLOAD_FOLDER'] = os.path.dirname(os.path.abspath(__file__))+'/static/upload/'
 
 # Dashboard Template
 from jinja2 import Environment, PackageLoader
 jinja_env = Environment(loader=PackageLoader('server', 'views'))
 
+# Check file upload
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+# Home
 @webapp.route("/")
 def home():
 
@@ -18,6 +31,11 @@ def home():
 
     data = {}
 
+    # Snapshot
+    snapdate = os.path.getmtime(webapp.config['UPLOAD_FOLDER']+SNAPSHOT_NAME)
+    data['image'] = {'updated': datetime.datetime.fromtimestamp(snapdate).strftime("%d/%m/%Y %H:%M:%S") }
+
+    # SCK api
     req = requests.get('https://api.smartcitizen.me/devices/3615').json()
 
     data['sck_id'] = req['id']
@@ -35,8 +53,26 @@ def home():
         if key:
             data[key] = {'value': round(sensor['value'],2), 'updated': dateutil.parser.parse(req['last_reading_at']).strftime("%d/%m/%Y %H:%M:%S")}
 
-
     return dashboard.render(data)
+
+# Upload snapshot
+@webapp.route("/snapshot", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'image' not in request.files:
+            return 'ERROR: No file..'
+
+        file = request.files['image']
+        if not file or file.filename == '' or not allowed_file(file.filename):
+            return 'ERROR: Wrong file..'
+
+        #filename = secure_filename(file.filename)
+        filename = SNAPSHOT_NAME
+        file.save(os.path.join(webapp.config['UPLOAD_FOLDER'], filename))
+        return 'SUCCESS!'
+    return 'ERROR: You\'re lost Dave..'
+
 
 
 if __name__ == "__main__":
